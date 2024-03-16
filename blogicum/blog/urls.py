ba592d4a05a0d867bf902d3edd5b8052af546e8a
Raul2455 <kirillmendrin245@gmail.com>
2024-03-16 10:42:04 +0300

    Поправил файл

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -7,13 +7,13 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path(
-        'posts/<int:pk>/', 
-        views.post_detail, 
+        'posts/<int:pk>/',
+        views.post_detail,
         name='post_detail'
     ),
     path(
-        'category/<slug:category_slug>/', 
-        views.category_posts, 
+        'category/<slug:category_slug>/',
+        views.category_posts,
         name='category_posts'
     ),
 ]
